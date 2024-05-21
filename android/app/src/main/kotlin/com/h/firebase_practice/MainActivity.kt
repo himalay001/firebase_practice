20626f77f6953fde93549e48259aec8b5fdaa3d0
Himalay Bhanderi <93080190+himalay001@users.noreply.github.com>
2024-05-21 16:01:19 +0530

    Connect firebase with flutter project

---
+++ android/app/src/main/kotlin/com/h/firebase_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.h.firebase_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/SHA_KEYS/sha.dart
@@ -0,0 +1,36 @@
+//> Task :app:signingReport
+// Variant: debug
+// Config: debug
+// Store: C:\Users\WELCOME\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: C9:D5:D7:94:B6:23:C0:52:CC:E2:8F:27:DF:84:BD:5F
+// SHA1: 10:00:3C:E9:52:F2:48:5C:12:86:ED:FA:90:FA:E5:62:9E:B2:57:DB
+// SHA-256: 4E:39:71:FE:43:06:6E:18:E4:0D:E9:CE:7E:7D:A5:CA:B2:E1:6D:3D:35:96:0D:7C:9A:3A:BE:B4:F2:8C:B8:D9
+// Valid until: Thursday, 14 September, 2051
+// ----------
+// Variant: release
+// Config: debug
+// Store: C:\Users\WELCOME\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: C9:D5:D7:94:B6:23:C0:52:CC:E2:8F:27:DF:84:BD:5F
+// SHA1: 10:00:3C:E9:52:F2:48:5C:12:86:ED:FA:90:FA:E5:62:9E:B2:57:DB
+// SHA-256: 4E:39:71:FE:43:06:6E:18:E4:0D:E9:CE:7E:7D:A5:CA:B2:E1:6D:3D:35:96:0D:7C:9A:3A:BE:B4:F2:8C:B8:D9
+// Valid until: Thursday, 14 September, 2051
+// ----------
+// Variant: profile
+// Config: debug
+// Store: C:\Users\WELCOME\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: C9:D5:D7:94:B6:23:C0:52:CC:E2:8F:27:DF:84:BD:5F
+// SHA1: 10:00:3C:E9:52:F2:48:5C:12:86:ED:FA:90:FA:E5:62:9E:B2:57:DB
+// SHA-256: 4E:39:71:FE:43:06:6E:18:E4:0D:E9:CE:7E:7D:A5:CA:B2:E1:6D:3D:35:96:0D:7C:9A:3A:BE:B4:F2:8C:B8:D9
+// Valid until: Thursday, 14 September, 2051
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\WELCOME\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: C9:D5:D7:94:B6:23:C0:52:CC:E2:8F:27:DF:84:BD:5F
+// SHA1: 10:00:3C:E9:52:F2:48:5C:12:86:ED:FA:90:FA:E5:62:9E:B2:57:DB
+// SHA-256: 4E:39:71:FE:43:06:6E:18:E4:0D:E9:CE:7E:7D:A5:CA:B2:E1:6D:3D:35:96:0D:7C:9A:3A:BE:B4:F2:8C:B8:D9
+// Valid until: Thursday, 14 September, 2051
